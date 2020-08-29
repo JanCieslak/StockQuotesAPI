c9882549b72463da7edc5cd6416a8a101eaecc7e
JanCieslak <s19096@pjwstk.edu.pl>
2020-08-29 20:47:48 +0200

    Add to dto class-validation

--- src/dtos/add-transation.dto.ts
+++ src/dtos/add-transation.dto.ts
@@ -1,9 +1,18 @@
+import { IsString, ValidateNested, IsInt, IsNumber } from 'class-validator';
+
 export class CompanyDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly symbol: string;
 }
 
 export class TransactionDto {
+  @ValidateNested()
   readonly company: CompanyDto;
+
+  @IsInt()
+  @IsNumber()
   readonly amount: number;
 }
