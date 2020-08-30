8fecc7f2c1ce16248650895d6f322c54488da09d
JanCieslak <s19096@pjwstk.edu.pl>
2020-08-30 18:03:49 +0200

    Configure swagger module

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('StockQuotes')
+    .setDescription('Api for stock quotes')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
