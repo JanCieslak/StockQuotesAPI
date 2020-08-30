1edf1ac399ecc89cc381fd020fd9d83077811a6e
JanCieslak <s19096@pjwstk.edu.pl>
2020-08-31 00:20:35 +0200

    Add transaction service tests

--- src/transactions/transactions.service.spec.ts
+++ src/transactions/transactions.service.spec.ts
@@ -15,4 +15,31 @@ describe('TransactionsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should add valid transaction to the database', async () => {
+    const companyName = 'TEST_COMPANY_NAME';
+    const companySymbol = 'TEST_COMPANY_SYMBOL';
+
+    const validRequest = `[ { "company": { "name": "${companyName}", "symbol": "${companySymbol}" }, "amount": 2200 } ]`;
+
+    // const falsyResult = await companyRepo.findOne({
+    //   name: companyName,
+    //   symbol: companySymbol,
+    // });
+    // expect(falsyResult).toBeFalsy();
+
+    await service.addTransactions(JSON.parse(validRequest));
+
+    // const truthlyResult = await companyRepo.findOne({
+    //   name: companyName,
+    //   symbol: companySymbol,
+    // });
+    // expect(truthlyResult).toBeTruthy();
+  });
+
+
+  it('should handle concurrent requests', () => {
+    // edge case same (new) company names
+
+  });
 });
