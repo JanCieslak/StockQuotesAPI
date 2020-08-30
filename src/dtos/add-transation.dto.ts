1f353bbdda7cfeff20fb6d09294761c0af26d7f6
JanCieslak <s19096@pjwstk.edu.pl>
2020-08-30 18:04:57 +0200

    Apply swagger deocrators
    Use swagger decorators on dtos and transaction controller's funciton

--- src/dtos/add-transation.dto.ts
+++ src/dtos/add-transation.dto.ts
@@ -1,33 +1,25 @@
-import {
-  IsString,
-  ValidateNested,
-  IsArray,
-  IsObject,
-  IsNumber,
-} from 'class-validator';
+import { IsString, ValidateNested, IsObject, IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CompanyDto {
+  @ApiProperty({ type: String, description: 'name' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ type: String, description: 'symbol' })
   @IsString()
   readonly symbol: string;
 }
 
 export class TransactionDto {
+  @ApiProperty({ type: CompanyDto, description: 'company' })
   @ValidateNested()
   @IsObject()
   @Type(() => CompanyDto)
   readonly company: CompanyDto;
 
+  @ApiProperty({ type: Number, description: 'amount' })
   @IsNumber()
   readonly amount: number;
 }
-
-// export class TransactionsDto {
-//   @ValidateNested({ each: true })
-//   @IsArray()
-//   @Type(() => TransactionDto)
-//   transactions: TransactionDto[];
-// }
