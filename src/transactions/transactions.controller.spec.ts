6c842f85c10c0aedc1363960c69ccff782806b0a
JanCieslak <s19096@pjwstk.edu.pl>
2020-09-01 13:27:48 +0200

    Finish testing transactions.controller

--- src/transactions/transactions.controller.spec.ts
+++ src/transactions/transactions.controller.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { mockConnection } from '../utils/test-helpers/mock-connection';
+import { Connection } from 'typeorm';
 import { TransactionsController } from './transactions.controller';
+import { TransactionsService } from './transactions.service';
 
 describe('TransactionsController', () => {
   let controller: TransactionsController;
+  let connection: Connection;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransactionsController],
+      providers: [
+        TransactionsService,
+        {
+          provide: Connection,
+          useFactory: mockConnection,
+        },
+      ],
     }).compile();
 
+    connection = module.get<Connection>(Connection);
     controller = module.get<TransactionsController>(TransactionsController);
+  });
+
+  afterAll(async () => {
+    await connection.close();
   });
 
   it('should be defined', () => {
